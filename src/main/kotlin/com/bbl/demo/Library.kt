bd1b103480bbf9ab1d286c86f7e6374e47fa040c
Riadh MNASRI <riadh.mnasri@gmail.com>
2019-05-21 18:18:04 +0200

    repetition 1 - second part

--- src/main/kotlin/com/bbl/demo/Library.kt
+++
@@ -1,9 +0,0 @@
-package com.bbl.demo
-
-class Library(val books: List<Book>) {
-
-    fun findBookByIsbn(isbn: String): Book?{
-        return books.firstOrNull() { it.isbn == isbn }
-    }
-
-}
--- src/main/kotlin/com/bbl/demo/Training.kt
+++ src/main/kotlin/com/bbl/demo/Training.kt
@@ -1,93 +1,44 @@
 package com.bbl.demo
 
-import com.bbl.demo.external.Book as BookDTO
-
 fun main() {
     // Declare variables
 
-    /* val kotlinBook: Book = Book("ISBNTEST1", "Programming Kotlin") */
-    //kotlinBook = null
-
 
     // Type inference
-    /* val price: Number = 100
-    fun increment(i: Int) = i+1 */
+
 
     // String quote and Triple Quotes
 
-    /* val text = "Hello"
-     val paragraph = """
-                    Hello
-                    World
-                    """
-     println("paragraph $paragraph") */
 
     // String templates
 
-    // Import renaming
 
-    /* val book = Book("ISBNTEST1", "Programming Kotlin")
-    val dtoBook = BookDTO(book.isbn, book.title)*/
+    // Import renaming
 
 
     // Ranges, step, reversed
 
-    /* val myRange = 1..10
-    val myRangeReversedbyTwo = (1..10).step(2).reversed()
-    myRangeReversedbyTwo.forEach {
-        println(">>>> $it")
-    }*/
-
     // Loops
-
-    /*val myRange = 1..10
-    for(i in myRange){
-        println(i)
-    }*/
-
+    
 
     // Instanciating classes
-    // Done
+
 
     // Equality
-    /*val kotlinBook: Book = Book("ISBNTEST1", "Programming Kotlin")
-    val kotlinBook2: Book = Book("ISBNTEST1", "Programming Kotlin")
-    println(kotlinBook==kotlinBook2)*/
-
 
     // Control flow as expressions
-    /* val kotlinBook: Book = Book("ISBNTEST1", "Programming Kotlin")
-    val result = if(kotlinBook.isAvailable()) "cool" else "may be next time !"
-    println("result :: $result")*/
 
     // Null safety - Safe Null access
-    /*val kotlinBook: Book = Book("ISBNTEST1", "Programming Kotlin")
-    val result = Library(listOf(kotlinBook)).findBookByIsbn("ISBNTEST1")
-    println("result :: ${result?.isbn}")*/
 
 
     // Elvis operator
-    /*val kotlinBook: Book = Book("ISBNTEST1", "Programming Kotlin")
-    val myDefaultFavoriteBook: Book = Book("ISBNTEST2", "Domain Driven Design")
-    val result = Library(listOf(kotlinBook)).findBookByIsbn("ISBNTEST1") ?: myDefaultFavoriteBook
-    println("result :: $result")*/
 
     // Force operation
-    /*val kotlinBook: Book = Book("ISBNTEST1", "Programming Kotlin")
-    val result = Library(listOf(kotlinBook)).findBookByIsbn("TOTO")
-    println("result :: ${result!!.isbn}")*/
 
-    // Smarts casts, explicits casts
-    /* fun printStringLenght(any: Any){
-        if(any is String){
-            println("any = [${any.length}]")
-        }
-    }
-    printStringLenght("Hello")
-     */
+    // Smarts casts, explicits casts //printStringLength
+
 
     // When expression with argument
-
 
 
     // When expression without argument
@@ -103,12 +54,6 @@ fun main() {
 
 
     // copy method
-    /*
-    val kotlinBook: Book = Book("ISBNTEST1", "Programming Kotlin")
-    val copiedBook = kotlinBook.copy("ISBNTEST2", "Programming RUST")
-    println("copied book :: $copiedBook")
-
-     */
 
 
     // enum classes
@@ -120,7 +65,7 @@ fun main() {
     // Inheritance // Payment, CardPayment(amount)
 
 
-    // Sealed classes
+    // Sealed classes //BookResult
 
 
     // Functions/ single expressions functions
@@ -144,82 +89,24 @@ fun main() {
     // Operator overloading
 
 
-    // Invoke
-
-    /*
-    val kotlibBook = Book("ISBN1", "Programming Kotlin")
-    val created = kotlibBook()
-    println("result:: $created")
-     */
+    // Invoke //Book
 
 
-    // Functions literals
+    // Functions literals //reverseCustom
 
-    /*
-    val reverseCustom: (String) -> String = {it.reversed()}
-    */
-
-
-    // Varargs and Spread operator
-
-    /*
-    val ints = intArrayOf(1,6,7,8,3)
-    fun displayInts(vararg ints: Int){
-        ints.asSequence().forEach {
-            println("$it")
-        }
-    }
-
-    displayInts(*ints, 5, 7, 8)
-    */
-
+    // Varargs and Spread operator //intArrayOf
 
 
     // Standard library functions //apply, let, with, run, lazy, use (try-with-resources), repeat
 
-    /* val kotlinBook: Book = Book("ISBNTEST1", "Programming Kotlin")
-    kotlinBook.apply { price = 30 }
-    kotlinBook.let { it.price=30  }
-    with(kotlinBook){
-        price = 30
-    }
-    val rustBook = lazy { newBook("ISBN3", "Programming rust")}
-    println("test")
-    println("result : ${rustBook.value.title}") */
+
+    // High order functions - function as a parameter //transform
 
 
-    // High order functions - function as a parameter
-
-    /*
-
-    fun transform(text: String, fn: (String) -> String): String{
-        return fn(text)
-    }
-
-    val result = transform("RIADH", {it.reversed()})
-    println("result :: $result")
-
-    */
-
-    // High order functions - return a function
-
-    /* fun reverse(): (String) -> String = {it.reversed()}
-    val myReverse = reverse()
-    val result = myReverse("RIADH")
-    println("result :: $result")
-
-     */
-
-
-
+    // High order functions - return a function //reverse
 
     // Properties
 
 
 }
-
-fun newBook(isbn: String, title: String): Book {
-    return Book(isbn, title)
-}
-
 
